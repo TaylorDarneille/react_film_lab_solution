6a98bf45381497f6d6bb8f307ef6886b82bbdbb3
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-09-12 06:52:21 -0700

    finished Part 3 of lab #2- previous commit message made in error, I had actually finished Part 2 at that point

--- src/Fave.js
+++ src/Fave.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
 class Fave extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isFave: false
+		}
+	}
 
 	handleClick = (e) => {
 		e.stopPropagation();
+		this.setState({isFave: this.state.isFave ? false : true});
 		console.log("handling Fave click!");
 	}
 
 	render() {
+		const toQueue = this.state.isFave ? "remove_from_queue" : "add_to_queue";
 		return(
-			<div className="film-row-fave add_to_queue" onClick={this.handleClick}>
+			<div className={`film-row-fave ${toQueue}`} onClick={this.handleClick}>
 	  			<p className="material-icons">add_to_queue</p>
 			</div>
 		)
